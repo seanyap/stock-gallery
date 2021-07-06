92466166b62bb547193614f0d43b13bedd293bd8
Sean Yap <syap0317@gmail.com>
2021-07-05 22:42:55 -0400

    include Image component for Images component

---
+++ src/components/Image.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Extracting image from props object using destructuring, thus don't need to use props.image
+const Image = ({ image, photographer }) => {
+  return (
+    <div className="image-container">
+      <a href={image.links.html} target="_blank" rel="noopener noreferrer">
+        <img className="single-image" src={image.urls.regular} alt="" />
+        <p className="attribution has-text-weight-light">
+          {photographer} | Unsplash
+        </p>
+      </a>
+    </div>
+  );
+};
+
+export default Image;
